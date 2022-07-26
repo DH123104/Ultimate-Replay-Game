be5068cb4f7c1495e456a8ec289b248aca542215
Danish H <109960339+DH123104@users.noreply.github.com>
2022-07-26 13:35:47 +0500

    Initial commit

---
+++ Assets/Scripts/ReplayStatistics.cs
@@ -0,0 +1,19 @@
+﻿using UltimateReplay.Statistics;
+using UnityEngine;
+
+namespace UltimateReplay
+{
+    /// </summary>
+    public class ReplayStatistics : MonoBehaviour
+    {
+        // Methods
+        public void OnGUI()
+        {
+            // Get memory usage
+            int memoryUsage = ReplayStorageTargetStatistics.CalculateReplayMemoryUsage();
+
+            GUILayout.Space(32);
+            GUILayout.Label("Replay Memory Usage: " + ReplayStatisticsUtil.GetMemorySizeSmallestUnit(memoryUsage) + ReplayStatisticsUtil.GetMemoryUnitString(memoryUsage));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Ultimate-Replay-Game
+ A simple and effective state based replay system for Unity
